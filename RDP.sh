f282fac71ff82fc5abd152ba028d6ae11352f534
Md. Rakibul Hassan Riad <67913904+rakibulriad@users.noreply.github.com>
2020-12-31 10:04:21 +0600

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -1,10 +1,6 @@
 #! /bin/bash
 printf "Installing RDP Be Patience... " >&2
 {
-sudo useradd -m ALOK
-sudo adduser ALOK sudo
-echo 'ALOK:8426' | sudo chpasswd
-sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
